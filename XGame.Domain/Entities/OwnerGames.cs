851d89fbfaeae8e15b41d15a3ec5b3587f20f222
Isadora Melo <isadora@rabbot.co>
2021-03-30 21:49:08 -0300

    Add project files.

---
+++ XGame.Domain/Entities/OwnerGames.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace XGame.Domain.Entities
+{
+    public class OwnerGames
+    {
+        public Guid Id { get; set; }
+        public JogoPlataforma JogoPlataforma { get; set; }
+        public bool Desejo { get; set; }
+
+        public DateTime DataDesejo { get; set; }
+        public bool Vendo { get; set; }
+        public bool Troco { get; set; }
+
+
+
+
+
+    }
+}
---
+++ XGame.Domain/Service/ServiceJogador.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using XGame.Domain.Arguments.Jogador;
+using XGame.Domain.Interfaces.Repositories;
+using XGame.Domain.Interfaces.Services;
+
+namespace XGame.Services
+{
+    public class ServiceJogador : IServiceJogador
+    {
+        private readonly IRepositoryJogador _repositoryJogador;
+
+        public ServiceJogador(IRepositoryJogador repositoryJogador)
+        {
+            _repositoryJogador = repositoryJogador;
+        }
+
+        public AdicionarJogadorResponse AdicionarJogador(AdicionarJogadorRequest request)
+        {
+
+            Guid id = _repositoryJogador.AdicionarJogador(request);
+
+            return new AdicionarJogadorResponse() { Id = id, Message = "Operação Realizada com sucesso!" };
+        } 
+
+        public AutenticarJogadorResponse AutenticarJogador(AutenticarJogadorRequest request)
+        {
+            if(request == null)
+            {
+                throw new Exception("AutenticarJogadorRquest é obrigatório"); 
+            }
+
+            if(string.IsNullOrEmpty(request.Email))
+            {
+                throw new Exception("Informe um email.");
+            }
+
+            if (string.IsNullOrEmpty(request.Senha))
+            {
+                throw new Exception("Informe a senha correta.");
+            }
+
+
+            if (request.Senha.Length < 6)
+            {
+                throw new Exception("Digite uma senha com o minímo de 6 caractéres.");
+            }
+
+            var response  = _repositoryJogador.AutenticarJogador(request);
+
+            return response;            
+        }
+    }
+}
